616fe0a1317017c5ff09e237fc992b63b4b70ee7
Jordan Schnur <actjay1898@gmail.com>
2022-10-05 23:13:36 -0400

    Formatting

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,9 @@ import './style.css'
 import app from './app'
 
 document.querySelector<HTMLDivElement>('#app').innerHTML = "";
-document.querySelector<HTMLDivElement>('#app').appendChild(app.view);
+document.querySelector<HTMLDivElement>('#app')?.append(app.view);
+window.addEventListener('resize', (e) => {
+	console.log(document.querySelector<HTMLDivElement>('#app'));
+	app.view.width = window.innerWidth;
+	app.view.height = window.innerHeight;
+});
